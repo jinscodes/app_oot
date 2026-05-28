d091cb8c33cda2fe1c34106328ae937b487449ba
Jay <jayhan0215@gmail.com>
2026-05-27 22:02:25 -0500

    Add Only One Touch text in auth landing screen

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -5,6 +5,6 @@ class AppColors {
 
   static const Color background = Color(0xFFF9F9F9);
   static const Color accent = Color(0xFFB17360);
-  static const Color textPrimary = Color(0xFF1F1F1F);
+  static const Color textPrimary = Color(0xFF2C2C2C);
   static const Color textSecondary = Color(0xFF5A5A5A);
 }

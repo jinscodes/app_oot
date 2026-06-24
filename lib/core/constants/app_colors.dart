8365e2718129af333e1a864585fceb34535193be
Jay <jayhan0215@gmail.com>
2026-06-24 01:44:03 -0500

    I changed the auth_landing screen

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -5,6 +5,8 @@ class AppColors {
 
   static const Color background = Color(0xFFF9F9F9);
   static const Color accent = Color(0xFFB17360);
+  static const Color primary = Color(0xFFD78F82);
+  static const Color termsBackground = Color(0xFFF9F5F1);
   static const Color textPrimary = Color(0xFF2C2C2C);
   static const Color textSecondary = Color(0xFF5A5A5A);
 }
